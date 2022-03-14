93751c83e9d9bc3b5e194e6f6577d187c739a6c9
Valeria297 <valeriserbin2907@gmail.com>
2022-03-14 17:59:55 +0300

    course_project_2022

--- app/src/main/java/com/example/hw_3/presentation/MainActivity.kt
+++ app/src/main/java/com/example/hw_3/presentation/MainActivity.kt
@@ -18,10 +18,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*WindowCompat.setDecorFitsSystemWindows(window, false)
-        window.statusBarColor = Color.TRANSPARENT
-        window.navigationBarColor = Color.TRANSPARENT*/
-
         AppCompatDelegate.setDefaultNightMode(
             when (sharedprefs.nightMode) {
                 NightMode.DARK -> AppCompatDelegate.MODE_NIGHT_YES
